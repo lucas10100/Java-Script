d4a2529db6e6fb3f0734724b1f50761a8473376b
lucas10100 <lucasdolores294@gmail.com>
2022-10-30 11:31:10 -0300

    adc

---
+++ teste.js/index.js
@@ -0,0 +1,8 @@
+let nome = prompt("digite um numero ");
+let idade= prompt ("outro numero");
+
+idade= Number(idade);
+nome=Number(nome);
+ const resultado = nome + idade;
+alert ("voce digitou" + resultado);
+
