88b305dccad9034c6c852c9116dfc648773c1bc3
lucas10100 <lucasdolores294@gmail.com>
2022-12-04 16:34:32 -0300

    #

--- teste.js/index.js
+++ teste.js/index.js
@@ -1,8 +1,10 @@
-let nome = prompt("digite um numero ");
-let idade= prompt ("outro numero");
 
-idade= Number(idade);
-nome=Number(nome);
- const resultado = nome + idade;
-alert ("voce digitou" + resultado);
-
+        const nome=prompt ('digite seu nome');
+        document.body.innerHTML += `seu nome é: ${nome} <br>`;
+        document.body.innerHTML += `seu nome tem : ${nome.length}  de letras <br>`;
+        document.body.innerHTML += `A segunda letra do seu nome é  : ${nome[1]}  <br>`;
+        document.body.innerHTML += `Qual é o primeiro indice  do seu nome : ${nome.indexOf('a')}  de letras <br>`;
+        document.body.innerHTML += `Qual é o ultimo indice  do seu nome : ${nome.lastIndexOf('a')}  de letras <br>`;
+        document.body.innerHTML += `as palavras do seu nome são : ${nome.split(' ')} <br>`;
+        document.body.innerHTML += `seu nome em letras maiusculas é  : ${nome.toUpperCase()}   <br>`;
+        document.body.innerHTML += `seu nome em letras minisculas  é : ${nome.toLowerCase()}   <br>`;
